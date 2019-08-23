1d612284ba783e83be55619685f4799715355595
Gabi B <Gab3009@Gmail.com>
2019-08-23 21:31:39 +0300

    configed env

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const PORT = 8080
 const api = require('./routes/api')
 const path = require('path')
-mongoose.connect('mongodb://localhost/CRM', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/CRM', {useNewUrlParser: true})
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
 
@@ -15,6 +15,6 @@ app.use('/', api)
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT, function () {
     console.log('listening on port: ' + PORT)
 })
